4fa44a6130e99a450b72e270ad79c6b1cba7eacb
Stefan Eekhof <stefaneekhof@aerialworks.ddns.net>
2021-03-24 19:39:06 +0100

    [ConnectiveHTTP] Fixed response headers

--- ConnectiveHTTP/src/main/java/org/asf/rats/HttpResponse.java
+++ ConnectiveHTTP/src/main/java/org/asf/rats/HttpResponse.java
@@ -130,8 +130,8 @@ public class HttpResponse {
 		});
 
 		resp.append("\r\n");
+		resp.append("\r\n");
 		if (intermediary != null) {
-			resp.append("\r\n");
 			strm.write(resp.toString().getBytes());
 			strm.write(intermediary.toByteArray());
 		} else {

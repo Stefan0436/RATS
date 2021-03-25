b4589cec0a310652c8fc6b8804636b5aba560531
Stefan Eekhof <stefaneekhof@aerialworks.ddns.net>
2021-03-25 17:17:04 +0100

    [RatsEventBus] Made the default event listeners interfaces

--- RatsEventBus/src/main/java/org/asf/rats/events/util/RatsEventListener.java
+++ RatsEventBus/src/main/java/org/asf/rats/events/util/RatsEventListener.java
@@ -3,18 +3,17 @@ package org.asf.rats.events.util;
 import java.util.Arrays;
 
 import org.asf.aos.util.service.extra.slib.smcore.SlibLogHandler;
-import org.asf.cyan.api.common.CyanComponent;
 import org.asf.cyan.api.events.core.IEventListener;
 
-public abstract class RatsEventListener extends CyanComponent implements IEventListener {
+public interface RatsEventListener extends IEventListener {
 
 	@Override
-	public void received(Object... params) {
+	public default void received(Object... params) {
 		if (params.length != 0 && params[0] instanceof SlibLogHandler) {
 			received((SlibLogHandler) params[0], Arrays.copyOfRange(params, 1, params.length));
 		}
 	}
 
-	public abstract void received(SlibLogHandler logger, Object... params);
+	public void received(SlibLogHandler logger, Object... params);
 
 }

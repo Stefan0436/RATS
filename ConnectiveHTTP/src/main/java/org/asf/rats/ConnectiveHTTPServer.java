98c0b443e981f13f2ec5b5b0ffccd73ea2e8e0c2
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-12-22 22:26:42 +0100

    Increase connective version

--- ConnectiveHTTP/src/main/java/org/asf/rats/ConnectiveHTTPServer.java
+++ ConnectiveHTTP/src/main/java/org/asf/rats/ConnectiveHTTPServer.java
@@ -60,7 +60,7 @@ public class ConnectiveHTTPServer extends CyanComponent {
 	}
 
 	protected String name = "ASF Connective";
-	protected String version = "1.0.0.A5";
+	protected String version = "1.0.0.A6";
 
 	protected boolean connected = false;
 	protected ServerSocket socket = null;
